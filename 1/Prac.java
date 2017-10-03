42de12d35809b755186817de5cc22db9bea66c0c
下村一将 <k-shimomura@milldea.com>
2017-10-03 20:17:48 +0900

    RPN is divided func

--- 1/Prac.java
+++ 1/Prac.java
@@ -3,46 +3,49 @@ import java.util.*;
 class Main {
 	public static void main(String[] args)
 	{
-		String	 str = "1 2 +";
-		String[] arr = str.split(" ", 0);
+		String str = "1 2 +";
+        solveAndPrintRPN(str);
+	}
+    
+    public static void solveAndPrintRPN(String expression) {
+        String[] arr = expression.split(" ", 0);
 
-		Deque <Integer> deque = new ArrayDeque <Integer>();
-
-		for(int i = 0; i < arr.length; i++)
-		{
-			int num;
-			try
-			{
+        Deque <Integer> deque = new ArrayDeque <Integer>();
+        for(int i = 0; i < arr.length; i++)
+        {
+            int num;
+            try
+            {
                 // arr[i]が数字だった場合
-				num = Integer.parseInt(arr[i]);
-				deque.push(num);
-			}
-			catch (NumberFormatException e)
-			{
-				// arr[i]が演算子だった場合
-				int num2 = deque.poll();
-				int num1 = deque.poll();
+                num = Integer.parseInt(arr[i]);
+                deque.push(num);
+            }
+            catch (NumberFormatException e)
+            {
+                // arr[i]が演算子だった場合
+                int num2 = deque.poll();
+                int num1 = deque.poll();
 
-				int ans=0;
-				switch(arr[i])
-				{
-				case "+":
-					ans = num1 + num2;
-					break;
-				case "-":
-					ans = num1 - num2;
-					break;
-				case "*":
-					ans = num1 * num2;
-					break;
-				case "/":
-					ans = num1 / num2;
-					break;
-				}
+                int ans=0;
+                switch(arr[i])
+                {
+                case "+":
+                    ans = num1 + num2;
+                    break;
+                case "-":
+                    ans = num1 - num2;
+                    break;
+                case "*":
+                    ans = num1 * num2;
+                    break;
+                case "/":
+                    ans = num1 / num2;
+                    break;
+                }
                 deque.push(ans);
-			}
-		}
+            }
+        }
         
         System.out.println(deque.poll());
-	}
+    }
 }
