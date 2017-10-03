31d68e427256bda6909f3f6a430cf7d99d92152e
下村一将 <k-shimomura@milldea.com>
2017-10-03 20:05:29 +0900

    solved 1

--- 1/Prac.java
+++ 1/Prac.java
@@ -3,11 +3,46 @@ import java.util.*;
 class Main {
 	public static void main(String[] args)
 	{
-        Deque<Integer> deque = new ArrayDeque<Integer>();
-        deque.push(1);
-        deque.push(2);    
+		String	 str = "1 2 +";
+		String[] arr = str.split(" ", 0);
+
+		Deque <Integer> deque = new ArrayDeque <Integer>();
+
+		for(int i = 0; i < arr.length; i++)
+		{
+			int num;
+			try
+			{
+                // arr[i]が数字だった場合
+				num = Integer.parseInt(arr[i]);
+				deque.push(num);
+			}
+			catch (NumberFormatException e)
+			{
+				// arr[i]が演算子だった場合
+				int num2 = deque.poll();
+				int num1 = deque.poll();
+
+				int ans=0;
+				switch(arr[i])
+				{
+				case "+":
+					ans = num1 + num2;
+					break;
+				case "-":
+					ans = num1 - num2;
+					break;
+				case "*":
+					ans = num1 * num2;
+					break;
+				case "/":
+					ans = num1 / num2;
+					break;
+				}
+                deque.push(ans);
+			}
+		}
         
-        System.out.println(deque.poll());
         System.out.println(deque.poll());
 	}
 }
