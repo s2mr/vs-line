d8df3b2c36860c44b44875d61f4baa18cbe77377
Kazumasa Shimomura <kazu.devapp@gmail.com>
2017-11-28 22:14:52 +0900

    store into tree

--- infix-notation-to-rpn/infix-notation-to-rpn.playground/Contents.swift
+++ infix-notation-to-rpn/infix-notation-to-rpn.playground/Contents.swift
@@ -1,55 +1,85 @@
 //: Playground - noun: a place where people can play
 
 import UIKit
+import Foundation
 
-struct Node {
-	var l: Int?
-	var r: Int?
-	var p: Int?
+class Node {
+	var l: Node?
+	var r: Node?
+	var p: Node?
 	var v: String?
 }
+
+//func pr(n: Node) {
+//	let l = n.l
+//	let r = n.r
+//
+//	while l != nil {
+//
+//	}
+//}
 
 extension Array where Element==Node {
 	func printN() {
 		for (i,e) in self.enumerated() {
-			print("node \(i): l = \(e.l ?? -1) \tr = \(e.r ?? -1)\tp = \(e.p ?? -1)\tv = \(e.v ?? "?")")
+			print("node \(i): l = \(e.l?.v ?? "?" ) \tr = \(e.r?.v ?? "?")\tp = \(e.p?.v ?? "?")\tv = \(e.v ?? "?")")
 		}
 	}
 }
 
-extension Character {
-	var asciiValue: Int {
-		get {
-			let s = String(self).unicodeScalars
-			return Int(s[s.startIndex].value)
-		}
+extension String {
+	subscript (i: Int) -> String {
+		return String(self[index(startIndex, offsetBy: i)])
 	}
 }
 
-var str = "3 * 4 + 2"
-str = str.replacingOccurrences(of: " ", with: "") // delete space
+//優先順位
+let dic: [String:Int] = ["(":4,
+						 "*":3,
+						 "/":3,
+						 "+":2,
+						 "-":2,
+						 ")":1]
 
-var nodes: [Node] = []
+var input = "3 * 4 + 2"
+input = input.replacingOccurrences(of: " ", with: "") // delete space
 
-//for s in str.unicodeScalars {
-//	nodes.append(Node(l: nil, r: nil, p: nil, v: String(s) ))
-//}
-//for (i,s) in str.unicodeScalars.enumerated() {
-//	if !(s >= "0" && s <= "9") {
-//		// 演算子
-//		nodes[i].l = i-1 // print(s)
-//		nodes[i].r = i+1
-//
-//		//数字
-//		nodes[i-1].p = i
-//		nodes[i+1].p = i
-//	}
-//}
-
-enum Arith: Int {
-	case plus = 1
-	case minus = 1
+func parseToTree(exp: String, node: Node?) -> Node {
+	var min: Int = 5
+	var index: Int = 0
+	let n = Node()
+	// 優先順位が低い演算子とそのインデックスを見つける
+	for (i,s) in exp.unicodeScalars.enumerated() {
+		if !(s >= "0" && s <= "9") {
+			let n = dic[String(s)]
+			if let n = n {
+				if min>n {
+					min = n
+					index = i
+				}
+			}
+		}
+	}
+	let f = exp.index(exp.startIndex, offsetBy: index)
+	let b = exp.index(f, offsetBy: 1)
+	let sf = exp[..<f] // 演算子の左側
+	let sc = exp[f]
+	let sb = exp[b..<exp.endIndex] // 演算子の右側
+	
+	if index == 0 {
+		// exp に演算子が含まれない場合
+		print("str: \(exp)")
+		n.v = exp
+		n.p = node
+	} else {
+		n.l = parseToTree(exp: String(sf), node: n)
+		n.r = parseToTree(exp: String(sb), node: n)
+		n.p = node
+		n.v = String(describing: sc)
+	}
+	
+	return n
 }
 
-
-nodes.printN()
+let n = parseToTree(exp: input, node: nil)
+dump(n)
