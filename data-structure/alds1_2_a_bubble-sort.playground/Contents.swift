de06dcf45b61ed5ce380fbe124c8e3cd1c100b07
Kazumasa Shimomura <kazu.devapp@gmail.com>
2017-12-03 18:38:15 +0900

    refactor

--- data-structure/alds1_2_a_bubble-sort.playground/Contents.swift
+++ data-structure/alds1_2_a_bubble-sort.playground/Contents.swift
@@ -1,15 +1,18 @@
 //: Playground - noun: a place where people can play
 
 import UIKit
+import XCTest
+
 
 func bubbleSort(A: inout [Int], N: Int) -> Int {
 	var flag = true
+	var i = 0 // 未ソート部分の開始位置（走査する範囲はどんどん狭まっていく）
 	var swapCount = 0
 	while flag {
 		flag = false
 		
 		var j = N-1
-		while j>0 {
+		while j>i {
 			if A[j] < A[j-1] {
 				A.swapAt(j, j-1)
 				flag = true
@@ -17,7 +20,9 @@ func bubbleSort(A: inout [Int], N: Int) -> Int {
 			}
 			j -= 1
 		}
+		i += 1
 	}
+	
 	return swapCount
 }
 
@@ -25,6 +30,8 @@ let input = """
 5
 5 3 2 4 1
 """
+
+let start = Date()
 
 let lines = input.split(separator: "\n")
 let N = Int(lines.first!)!
@@ -37,4 +44,11 @@ for i in 0..<A.count-1 {
 }
 print(A.last!)
 print(C)
+
+let elapsed = Date().timeIntervalSince(start)
+print(elapsed)
+
+// 0.00886297225952148
+// 0.00757294893264771 （iを利用して)
+
 
